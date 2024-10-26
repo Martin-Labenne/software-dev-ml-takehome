02b293637f05d79a1c032c96cb5ba1a0a8580e63
Martin-Labenne <martinlabenne@gmail.com>
2024-10-26 20:06:04 +0200

    create queries file

---
+++ src/queries.py
@@ -0,0 +1,30 @@
+import polars as pl 
+
+def top_100_avg_kills_per_operator_per_match(df: pl.LazyFrame) -> pl.LazyFrame: 
+    return (
+        df.group_by('match_id', 'operator_id')
+        .agg(
+            pl.col('nb_kills')
+            .mean()
+        )
+        .sort(
+            'nb_kills', 
+            descending=True
+        )
+        .group_by('operator_id')
+        .head(100)
+    )
+
+def partition_by_match_prefix(df: pl.LazyFrame) -> pl.LazyFrame: 
+    return (
+        df.with_columns(
+            pl.col('match_id')
+            .str.slice(0,3)
+            .alias('match_prefix')
+        )
+        .group_by('match_prefix')
+        .agg([
+            pl.col(col_name) 
+            for col_name in ['player_id', 'match_id', 'operator_id', 'nb_kills']
+        ])
+    )
